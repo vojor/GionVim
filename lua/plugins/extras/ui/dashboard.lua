57ee9c87c74998001d2414dc92136df436abf881
legbor <inveniculo@gmail.com>
2024-04-14 22:17:40 +0800

    dashboard add emoji

--- lua/plugins/extras/ui/dashboard.lua
+++ lua/plugins/extras/ui/dashboard.lua
@@ -84,7 +84,14 @@ return {
                         local stats = require("lazy").stats()
                         local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
                         return {
-                            "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms",
+                            "⚡ Neovim loaded "
+                                .. stats.loaded
+                                .. "/"
+                                .. stats.count
+                                .. " plugins in "
+                                .. ms
+                                .. "ms"
+                                .. " 😀😀😀",
                         }
                     end,
                 },
