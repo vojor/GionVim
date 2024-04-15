0279a31ba0c94e862773fc57687344268c5403f5
legbor <inveniculo@gmail.com>
2024-04-16 00:39:21 +0800

    adjust comment

--- lua/boot/bootstrap.lua
+++ lua/boot/bootstrap.lua
@@ -15,82 +15,83 @@ _G.GionVim = require("config.util")
 
 require("lazy").setup({
     spec = {
-        ---- 导入基础插件 ----
+        ---- Basic plugin ----
         { import = "plugins" },
-        ---- 导入额外扩展插件 ----
+        ---- Extras plugin ----
         -- colorscheme
         -- { import = "plugins.extras.colorscheme.kanagawa" },
         -- { import = "plugins.extras.colorscheme.monokaipro" },
         -- { import = "plugins.extras.colorscheme.nightfox" },
         -- { import = "plugins.extras.colorscheme.onenord" },
         -- dap
-        { import = "plugins.extras.dap.luadap" }, -- 特定语言调试插件
+        { import = "plugins.extras.dap.luadap" }, -- neovim lua debug
         -- develop
-        { import = "plugins.extras.develop.bigfile" }, -- 大文件控制
-        { import = "plugins.extras.develop.comments" }, -- 快速注释
-        { import = "plugins.extras.develop.compiler" }, -- 构建和运行代码
-        { import = "plugins.extras.develop.font-icon" }, -- 图标选择
-        { import = "plugins.extras.develop.commander" }, -- 查找命令以及按键绑定
+        { import = "plugins.extras.develop.bigfile" }, -- big file control
+        { import = "plugins.extras.develop.comments" }, -- quick comment
+        { import = "plugins.extras.develop.compiler" }, -- Build and run code
+        { import = "plugins.extras.develop.font-icon" }, -- Select icon
+        { import = "plugins.extras.develop.commander" }, -- Find plugin keymaps
         -- diagnostics
-        { import = "plugins.extras.diagnostics.trouble" }, -- 代码诊断结果
+        { import = "plugins.extras.diagnostics.trouble" }, -- Look diagnostics result
         -- editor
-        { import = "plugins.extras.editor.autotools" }, -- 自动处理工具
-        { import = "plugins.extras.editor.replacer" }, -- 参数替换
-        { import = "plugins.extras.editor.portal" }, -- 位置列表导航
-        { import = "plugins.extras.editor.surround" }, -- 修改定界符
-        -- { import = "plugins.extras.editor.coerce" }, -- 更改关键词大小写
-        -- { import = "plugins.extras.develop.refactor" }, -- 代码重构
+        { import = "plugins.extras.editor.autotools" }, -- Auto pairs save and go lastplace
+        { import = "plugins.extras.editor.replacer" }, -- Factor replace
+        { import = "plugins.extras.editor.portal" }, -- Local list navigate
+        { import = "plugins.extras.editor.surround" }, -- Surround modify
+        -- { import = "plugins.extras.editor.coerce" }, -- Keyword capitalization conversion
+        -- { import = "plugins.extras.develop.refactor" }, -- Refactor code
         -- elevate
-        { import = "plugins.extras.elevate.scope" }, -- 增强选项卡范围
-        { import = "plugins.extras.elevate.escape" }, -- 更好的模式切换
-        { import = "plugins.extras.elevate.scroll" }, -- 平滑滚动
-        { import = "plugins.extras.elevate.substitute" }, -- 代替内置 substitute
+        { import = "plugins.extras.elevate.scope" }, -- Enhace Buffer scope
+        { import = "plugins.extras.elevate.escape" }, -- More user-friendly mode switching
+        { import = "plugins.extras.elevate.scroll" }, -- Smooth scrolling
+        { import = "plugins.extras.elevate.substitute" }, -- Replace neovim built-in 'substitute'
         -- explorer
-        { import = "plugins.extras.explorer" }, -- 更多文件浏览工具
+        { import = "plugins.extras.explorer" }, -- More explorer
         -- git
-        { import = "plugins.extras.git" }, -- 优秀 git 工具
+        { import = "plugins.extras.git" }, -- Improve git
         -- labeler
-        { import = "plugins.extras.labeler.harpoon" }, -- 文件标记
-        { import = "plugins.extras.labeler.marks" }, -- 行标记
-        -- { import = "plugins.extras.labeler.trailblazer" }, -- 字符标记
+        { import = "plugins.extras.labeler.harpoon" }, -- File mark
+        { import = "plugins.extras.labeler.marks" }, -- Line mark
+        -- { import = "plugins.extras.labeler.trailblazer" }, -- Char mark
         -- line
-        -- { import = "plugins.extras.line.sideline" }, -- 侧边栏
-        -- { import = "plugins.extras.line.statusLine" }, -- 状态栏
-        -- { import = "plugins.extras.line.tabline" }, -- tabline
-        -- lsp lang
-        { import = "plugins.extras.lsp.lang.clangd" },
-        -- { import = "plugins.extras.lsp.lang.java" },
-        { import = "plugins.extras.lsp.lang.jqx" },
-        { import = "plugins.extras.lsp.lang.typescript" },
-        -- lsp strength
-        { import = "plugins.extras.lsp.reinforce.aerial" }, -- 代码大纲
-        -- { import = "plugins.extras.lsp.reinforce.dropbar" }, -- 代码导航
-        { import = "plugins.extras.lsp.reinforce.fidget" }, -- LSP 进度提示
-        { import = "plugins.extras.lsp.reinforce.illuminate" }, -- 显示光标下相同词汇
-        { import = "plugins.extras.lsp.reinforce.increname" }, -- 基于 LSP 带有即时视觉反馈的重命名
-        { import = "plugins.extras.lsp.reinforce.lspsaga" }, -- 改善 Neovim LSP 体验
-        { import = "plugins.extras.lsp.reinforce.naybuddy" }, -- 浮动导航窗口
-        { import = "plugins.extras.lsp.reinforce.outline" }, -- 符号大纲
+        -- { import = "plugins.extras.line.sideline" },
+        -- { import = "plugins.extras.line.statusLine" },
+        -- { import = "plugins.extras.line.tabline" },
+        -- lsp elevate
+        -- -- lang
+        { import = "plugins.extras.lsp.lang.clangd" }, -- C/C++
+        -- { import = "plugins.extras.lsp.lang.java" }, -- Java
+        { import = "plugins.extras.lsp.lang.jqx" }, -- json/yaml
+        { import = "plugins.extras.lsp.lang.typescript" }, -- javascript/typescript
+        -- -- util
+        { import = "plugins.extras.lsp.reinforce.aerial" }, -- Code outline
+        -- { import = "plugins.extras.lsp.reinforce.dropbar" }, -- Code navigate
+        { import = "plugins.extras.lsp.reinforce.fidget" }, -- Progress prompt
+        { import = "plugins.extras.lsp.reinforce.illuminate" }, -- Display the same vocabulary under the cursor
+        { import = "plugins.extras.lsp.reinforce.increname" }, -- Timely feedback rename
+        { import = "plugins.extras.lsp.reinforce.lspsaga" }, -- Strengthen LSP experience
+        { import = "plugins.extras.lsp.reinforce.naybuddy" }, -- Float navigate
+        { import = "plugins.extras.lsp.reinforce.outline" }, -- Symbols Outline
         -- search
-        { import = "plugins.extras.fzf" }, -- fzf 搜索
+        { import = "plugins.extras.fzf" },
         -- treesitter
-        { import = "plugins.extras.treesitter.treesj" }, -- 分割/合并代码
-        { import = "plugins.extras.treesitter.context" }, -- 上下文环境
-        { import = "plugins.extras.treesitter.autotag" }, -- 自动关闭标签
-        { import = "plugins.extras.treesitter.commentstring" }, -- 注释
+        { import = "plugins.extras.treesitter.treesj" }, -- Splitting or merging code
+        { import = "plugins.extras.treesitter.context" },
+        { import = "plugins.extras.treesitter.autotag" }, -- Auto close tag
+        { import = "plugins.extras.treesitter.commentstring" },
         -- ui
         -- { import = "plugins.extras.ui.alpha" },
         -- { import = "plugins.extras.ui.starter" },
-        { import = "plugins.extras.ui.dashboard" }, -- 启动界面
-        -- { import = "plugins.extras.ui.animate" }, -- 动画效果
-        -- { import = "plugins.extras.ui.annotbox" }, -- 注释美化
-        { import = "plugins.extras.ui.colorizer" }, -- 16 进制颜色
-        { import = "plugins.extras.ui.edgy" }, -- 预定义布局
-        { import = "plugins.extras.ui.hlslens" }, -- 搜索高亮条目
-        { import = "plugins.extras.ui.rainbow" }, -- 彩虹括号
-        { import = "plugins.extras.ui.indentstyle" }, -- 缩进样式
+        { import = "plugins.extras.ui.dashboard" }, -- Interface
+        -- { import = "plugins.extras.ui.animate" }, -- Animate
+        -- { import = "plugins.extras.ui.annotbox" }, -- Beautiful comment
+        { import = "plugins.extras.ui.colorizer" }, -- Display hexadecimal colors
+        { import = "plugins.extras.ui.edgy" }, -- Predefined layout
+        { import = "plugins.extras.ui.hlslens" }, -- Highlight entries during search
+        { import = "plugins.extras.ui.rainbow" },
+        { import = "plugins.extras.ui.indentstyle" },
         -- utils
-        { import = "plugins.extras.utils.startuptime" }, -- 测量启动时间
+        { import = "plugins.extras.utils.startuptime" }, -- Measure startup time
     },
     defaults = { version = false },
     install = { colorscheme = { "tokyonight", "nightfox", "habamax" } },
