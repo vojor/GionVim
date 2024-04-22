b51c98103e429af917ca24e20c5fcf9f3a6a9a7d
legbor <inveniculo@gmail.com>
2024-04-22 16:37:02 +0800

    feat: add more filetypes

---
+++ lua/plugins/extras/utils/dot.lua
@@ -0,0 +1,39 @@
+local xdg_config = vim.env.XDG_CONFIG_HOME or vim.env.HOME .. "/.config"
+
+local function have(path)
+    return vim.uv.fs_stat(xdg_config .. "/" .. path) ~= nil
+end
+
+return {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = function(_, opts)
+            local function add(lang)
+                if type(opts.ensure_installed) == "table" then
+                    table.insert(opts.ensure_installed, lang)
+                end
+            end
+
+            vim.filetype.add({
+                extension = { rasi = "rasi", rofi = "rasi", wofi = "rasi" },
+                filename = {
+                    [".env"] = "dotenv",
+                    ["vifmrc"] = "vim",
+                },
+                pattern = {
+                    ["%.env%.[%w_.-]+"] = "dotenv",
+                },
+            })
+
+            add("git_config")
+
+            if have("fish") then
+                add("fish")
+            end
+
+            if have("rofi") or have("wofi") then
+                add("rasi")
+            end
+        end,
+    },
+}
