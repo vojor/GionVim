5ac698e1146dca249c01062933bba1991f64348f
legbor <inveniculo@gmail.com>
2024-06-05 21:34:38 +0800

    perf: use nvim-rip-substitute replace nvim-alt-substitute

--- lua/gionvim/plugins/extras/elevate/substitute.lua
+++ lua/gionvim/plugins/extras/elevate/substitute.lua
@@ -1,19 +1,18 @@
 return {
 
     {
-        "chrisgrieser/nvim-alt-substitute",
+        "chrisgrieser/nvim-rip-substitute",
         lazy = true,
         keys = {
-            { "<leader>sa", mode = { "n", "x" }, desc = "󱗘 :AltSubstitute" },
-            { "<leader>sA", mode = { "n", "x" }, desc = "󱗘 :AltSubstitute (word under cursor)" },
+            {
+                "<leader>sr",
+                function()
+                    require("rip-substitute").sub()
+                end,
+                mode = { "n", "x" },
+                desc = " rip substitute",
+            },
         },
         dependencies = { "dressing.nvim" },
-        config = function()
-            require("alt-substitute").setup()
-            vim.keymap.set({ "n", "x" }, "<leader>sa", [[:S ///g<Left><Left><Left>]])
-            vim.keymap.set({ "n", "x" }, "<leader>sA", function()
-                return ":S /" .. vim.fn.expand("<cword>") .. "//g<Left><Left>"
-            end, { expr = true })
-        end,
     },
 }
