03b38e98b9993a57f662496369850357a9db5a21
legbor <inveniculo@gmail.com>
2024-06-27 22:05:55 +0800

    feat: imrpove bigfile support

--- lua/gionvim/core/autocmds.lua
+++ lua/gionvim/core/autocmds.lua
@@ -78,7 +78,8 @@ vim.filetype.add({
     pattern = {
         [".*"] = {
             function(path, buf)
-                return vim.bo[buf].filetype ~= "bigfile"
+                return vim.bo[buf]
+                        and vim.bo[buf].filetype ~= "bigfile"
                         and path
                         and vim.fn.getfsize(path) > vim.g.bigfile_size
                         and "bigfile"
