c3f8ac5eef47dbf9bfc3aa34ca41128799824ed0
legbor <inveniculo@gmail.com>
2024-06-27 22:05:32 +0800

    refactor: lazy.nvim setup

--- lua/gionvim/boot/bootstrap.lua
+++ lua/gionvim/boot/bootstrap.lua
@@ -1,15 +1,9 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable",
-        lazypath,
-    })
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 end
-vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+vim.opt.rtp:prepend(lazypath)
 
 _G.gionvim_docs = true
 _G.GionVim = require("gionvim.util")
