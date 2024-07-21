604cde89b57598aa4bc009ba18932e404496b10c
legbor <inveniculo@gmail.com>
2024-07-21 18:06:26 +0800

    feat: add grug-far.nvim

--- lua/gionvim/plugins/editor.lua
+++ lua/gionvim/plugins/editor.lua
@@ -122,88 +122,27 @@ return {
     },
     -- Text replace
     {
-        "nvim-pack/nvim-spectre",
-        build = false,
+        "MagicDuck/grug-far.nvim",
         lazy = true,
-        cmd = "Spectre",
+        cmd = "GrugFar",
+        opts = {
+            headerMaxWidth = 80,
+        },
         keys = {
             {
-                "<leader>rg",
+                "<leader>rR",
                 function()
-                    require("spectre").toggle()
+                    local is_visual = vim.fn.mode():lower():find("v")
+                    if is_visual then
+                        vim.cmd([[normal! v]])
+                    end
+                    local grug = require("grug-far");
+                    (is_visual and grug.with_visual_selection or grug.grug_far)({
+                        prefills = { filesFilter = "*." .. vim.fn.expand("%:e") },
+                    })
                 end,
-                desc = "Toggle Spectre",
-            },
-            {
-                "<leader>rw",
-                function()
-                    require("spectre").open_visual({ select_word = true })
-                end,
-                desc = "Search Current Word",
-            },
-            {
-                "<leader>rw",
-                function()
-                    require("spectre").open_visual()
-                end,
-                mode = "v",
-                desc = "Search Current Word",
-            },
-            {
-                "<leader>rt",
-                function()
-                    require("spectre").open_file_search({ select_word = true })
-                end,
-                desc = "Search On Current File",
-            },
-        },
-        opts = {
-            open_cmd = "noswapfile vnew",
-            live_update = true,
-            is_block_ui_break = true,
-            mapping = {
-                ["toggle_line"] = {
-                    map = "<leader>ra",
-                },
-                ["enter_file"] = {
-                    map = "<CR>",
-                },
-                ["send_to_qf"] = {
-                    map = "<leader>rq",
-                },
-                ["replace_cmd"] = {
-                    map = "<leader>rd",
-                },
-                ["show_option_menu"] = {
-                    map = "<leader>re",
-                },
-                ["run_current_replace"] = {
-                    map = "<leader>rr",
-                },
-                ["run_replace"] = {
-                    map = "<leader>rp",
-                },
-                ["change_view_mode"] = {
-                    map = "<leader>ri",
-                },
-                ["change_replace_sed"] = {
-                    map = "<leader>ru",
-                },
-                ["change_replace_oxi"] = {
-                    map = "<leader>rx",
-                },
-                ["toggle_live_update"] = {
-                    map = "<leader>rj",
-                },
-                ["toggle_ignore_case"] = {
-                    map = "<leader>rk",
-                },
-                ["toggle_ignore_hidden"] = {
-                    map = "<leader>rs",
-                },
-                ["resume_last_search"] = {
-                    map = "<leader>rl",
-                },
+                mode = { "n", "v" },
+                desc = "Search and Replace",
             },
         },
     },
--- lua/gionvim/plugins/extras/editor/replacer.lua
+++ lua/gionvim/plugins/extras/editor/replacer.lua
@@ -10,9 +10,89 @@ return {
         opts = {},
     },
     {
-        "MagicDuck/grug-far.nvim",
+        "nvim-pack/nvim-spectre",
+        build = false,
         lazy = true,
-        cmd = "GrugFar",
-        opts = {},
+        cmd = "Spectre",
+        keys = {
+            {
+                "<leader>rg",
+                function()
+                    require("spectre").toggle()
+                end,
+                desc = "Toggle Spectre",
+            },
+            {
+                "<leader>rw",
+                function()
+                    require("spectre").open_visual({ select_word = true })
+                end,
+                desc = "Search Current Word",
+            },
+            {
+                "<leader>rw",
+                function()
+                    require("spectre").open_visual()
+                end,
+                mode = "v",
+                desc = "Search Current Word",
+            },
+            {
+                "<leader>rt",
+                function()
+                    require("spectre").open_file_search({ select_word = true })
+                end,
+                desc = "Search On Current File",
+            },
+        },
+        opts = {
+            open_cmd = "noswapfile vnew",
+            live_update = true,
+            is_block_ui_break = true,
+            mapping = {
+                ["toggle_line"] = {
+                    map = "<leader>ra",
+                },
+                ["enter_file"] = {
+                    map = "<CR>",
+                },
+                ["send_to_qf"] = {
+                    map = "<leader>rq",
+                },
+                ["replace_cmd"] = {
+                    map = "<leader>rd",
+                },
+                ["show_option_menu"] = {
+                    map = "<leader>re",
+                },
+                ["run_current_replace"] = {
+                    map = "<leader>rr",
+                },
+                ["run_replace"] = {
+                    map = "<leader>rp",
+                },
+                ["change_view_mode"] = {
+                    map = "<leader>ri",
+                },
+                ["change_replace_sed"] = {
+                    map = "<leader>ru",
+                },
+                ["change_replace_oxi"] = {
+                    map = "<leader>rx",
+                },
+                ["toggle_live_update"] = {
+                    map = "<leader>rj",
+                },
+                ["toggle_ignore_case"] = {
+                    map = "<leader>rk",
+                },
+                ["toggle_ignore_hidden"] = {
+                    map = "<leader>rs",
+                },
+                ["resume_last_search"] = {
+                    map = "<leader>rl",
+                },
+            },
+        },
     },
 }
