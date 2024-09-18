b6902ef551f8556adec77f353eb3ed58876179ff
Smith Alike <inveniculo@gmail.com>
2024-09-18 22:09:31 +0800

    cmp add function

--- lua/gionvim/plugins/coding.lua
+++ lua/gionvim/plugins/coding.lua
@@ -92,6 +92,8 @@ return {
                 mapping = cmp.mapping.preset.insert({
                     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
                     ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                    ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+                    ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
                     ["<C-Space>"] = cmp.mapping.complete(),
                     ["<C-e>"] = cmp.mapping.abort(),
                     ["<CR>"] = cmp.mapping.confirm({ select = true }),
