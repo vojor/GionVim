82e9027c0b156cb6bab39b7c2be467d4678c561a
Smith Alike <inveniculo@gmail.com>
2024-11-16 20:04:11 +0800

    fix pwsh

--- lua/gionvim/util/terminal.lua
+++ lua/gionvim/util/terminal.lua
@@ -13,11 +13,11 @@ function M.setup(shell)
         end
 
         vim.o.shellcmdflag =
-            "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';"
+            "-NoLogo -NonInteractive -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';$PSStyle.OutputRendering='plaintext';Remove-Alias -Force -ErrorAction SilentlyContinue tee;"
 
         vim.o.shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
 
-        vim.o.shellpipe = '2>&1 | %%{ "$_" } | Tee-Object %s; exit $LastExitCode'
+        vim.o.shellpipe = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode'
 
         vim.o.shellquote = ""
         vim.o.shellxquote = ""
