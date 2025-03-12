d71de5dcc5d12084c64663dc16fa845c5b717c98
Smith Alike <sojenir@gmail.com>
2025-03-13 03:09:58 +0800

    update

--- lua/gionvim/plugins/extras/treesitter/autotag.lua
+++ lua/gionvim/plugins/extras/treesitter/autotag.lua
@@ -2,13 +2,10 @@ return {
     {
         "windwp/nvim-ts-autotag",
         lazy = true,
-        ft = { "xml", "html", "markdown", "javascript", "typescript", "javascriptreact", "typescriptreact" },
+        ft = { "xml", "html", "markdown", "javascript", "typescript" },
         config = function()
             require("nvim-ts-autotag").setup({
                 opts = {
-                    enable = true,
-                    enable_rename = true,
-                    enable_close = true,
                     enable_close_on_slash = true,
                 },
             })
