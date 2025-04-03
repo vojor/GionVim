d4381a21023ebf503259c85e9feb1ffa237f9e56
Smith Alike <sojenir@gmail.com>
2025-04-03 20:10:01 +0800

    remove abolish config

--- lua/gionvim/plugins/formatter.lua
+++ lua/gionvim/plugins/formatter.lua
@@ -1,14 +1,3 @@
-local function first(bufnr, ...)
-    local conform = require("conform")
-    for i = 1, select("#", ...) do
-        local formatter = select(i, ...)
-        if conform.get_formatter_info(formatter, bufnr).available then
-            return formatter
-        end
-    end
-    return select(1, ...)
-end
-
 return {
     -- Format
     {
@@ -65,7 +54,7 @@ return {
                 if vim.b[bufnr].disable_autoformat then
                     return
                 end
-                return { timeout_ms = 3000, lsp_format = "fallback" }
+                return { timeout_ms = 3000 }
             end,
             formatters = {
                 injected = {
