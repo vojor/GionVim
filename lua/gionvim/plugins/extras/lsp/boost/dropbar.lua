1ca44bd46ea93105e242020f60c85b0083191d53
Smith Alike <sojenir@gmail.com>
2025-04-21 19:30:59 +0800

    update

--- lua/gionvim/plugins/extras/lsp/boost/dropbar.lua
+++ lua/gionvim/plugins/extras/lsp/boost/dropbar.lua
@@ -7,7 +7,7 @@ return {
         config = function()
             local dropbar_api = require("dropbar.api")
 
-            vim.keymap.set("n", "<leader>P", dropbar_api.pick, { desc = "Pick symbols in winbar" })
+            vim.keymap.set("n", "<leader>sp", dropbar_api.pick, { desc = "Pick symbols in winbar" })
             vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
             vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
         end,
