3573d76569169e1e6e27b468382530e19ac85e13
Smith Alike <sojenir@gmail.com>
2025-04-22 00:21:52 +0800

    use snacks bigfile manipulate

--- lua/gionvim/plugins/treesitter.lua
+++ lua/gionvim/plugins/treesitter.lua
@@ -61,14 +61,6 @@ return {
             ignore_install = {},
             highlight = {
                 enable = true,
-                -- If file size outweigh 2M, disable treesitter
-                disable = function(lang, buf)
-                    local max_filesize = 2000 * 1024
-                    local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
-                    if ok and stats and stats.size > max_filesize then
-                        return true
-                    end
-                end,
                 additional_vim_regex_highlighting = false,
             },
             incremental_selection = {
