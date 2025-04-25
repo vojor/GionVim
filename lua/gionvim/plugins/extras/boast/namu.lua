16823c653a8e8b47bf4ac65822ed96cc6ba50865
Smith Alike <sojenir@gmail.com>
2025-04-25 12:44:02 +0800

    add some namu.nvim keys

--- lua/gionvim/plugins/extras/boast/namu.lua
+++ lua/gionvim/plugins/extras/boast/namu.lua
@@ -4,11 +4,15 @@ return {
         lazy = true,
         cmd = "Namu",
         keys = {
-            { "<leader>nn", "<cmd>Namu symbols<CR>", desc = "Jump to LSP symbol" },
+            { "<leader>nn", "<cmd>Namu symbols<CR>", desc = "Jump to LSP symbols" },
             { "<leader>nw", "<cmd>Namu workspace<CR>", desc = "LSP Symbols - Workspace" },
+            { "<leader>nt", "<cmd>Namu watchtower<CR>", desc = "Lsp Symbols - fallback treesitter" },
+            { "<leader>nd", "<cmd>Namu diagnostics<CR>", desc = "Diagnostics - Workspace" },
+            { "<leader>nr", "<cmd>Namu treesitter<CR>", desc = "Treesitter Symbols" },
+            { "<leader>nh", "<cmd>Namu help<CR>", desc = "Namu Help" },
         },
         opts = {
-            ui_select = { enable = true },
+            ui_select = { enable = false },
         },
     },
 }
