9652dfd2eae7d79124aabcc6443ee67769edb0d7
Smith Alike <sojenir@gmail.com>
2025-05-16 21:45:46 +0800

    perf: edgy free toggleterm

--- lua/gionvim/plugins/extras/ui/edgy.lua
+++ lua/gionvim/plugins/extras/ui/edgy.lua
@@ -22,13 +22,6 @@ return {
             local opts = {
                 bottom = {
                     {
-                        ft = "toggleterm",
-                        size = { height = 0.4 },
-                        filter = function(buf, win)
-                            return vim.api.nvim_win_get_config(win).relative == ""
-                        end,
-                    },
-                    {
                         ft = "noice",
                         size = { height = 0.4 },
                         filter = function(buf, win)
