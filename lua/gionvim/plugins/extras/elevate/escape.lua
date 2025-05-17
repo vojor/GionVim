f52fdb08bf4b2b4b6e342520c893cdf457d8af02
Smith Alike <sojenir@gmail.com>
2025-05-17 15:50:57 +0800

    feat(plugin): Use mini.keymap replace better-escape.nvim

--- lua/gionvim/plugins/extras/elevate/escape.lua
+++ lua/gionvim/plugins/extras/elevate/escape.lua
@@ -1,8 +1,19 @@
 return {
     {
-        "max397574/better-escape.nvim",
+        "echasnovski/mini.keymap",
+        version = false,
         lazy = true,
-        event = "InsertEnter",
-        opts = {},
+        event = { "InsertEnter", "CmdlineEnter" },
+        config = function()
+            local mode = { "i", "c", "x", "s" }
+            local map_combo = require("mini.keymap").map_combo
+
+            map_combo(mode, "jk", "<BS><BS><Esc>")
+
+            map_combo(mode, "kj", "<BS><BS><Esc>")
+
+            map_combo("t", "jk", "<BS><BS><C-\\><C-n>")
+            map_combo("t", "kj", "<BS><BS><C-\\><C-n>")
+        end,
     },
 }
