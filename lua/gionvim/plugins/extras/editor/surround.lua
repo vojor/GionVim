0fbda56006361b6e69ebe3c060b784e132f4f980
Smith Alike <sojenir@gmail.com>
2026-02-28 19:57:59 +0800

    perf: biome update to v2.3.15

--- lua/gionvim/plugins/extras/editor/surround.lua
+++ lua/gionvim/plugins/extras/editor/surround.lua
@@ -1,23 +1,9 @@
 return {
     {
         "kylechui/nvim-surround",
-        version = "*",
+        branch = "main",
         lazy = true,
         event = "LazyFile",
-        opts = {
-            keymaps = {
-                insert = "<C-g>s",
-                insert_line = "<C-g>S",
-                normal = "ys",
-                normal_cur = "yss",
-                normal_line = "yS",
-                normal_cur_line = "ySS",
-                visual = "gs",
-                visual_line = "gS",
-                delete = "ds",
-                change = "cs",
-                change_line = "cS",
-            },
-        },
+        opts = {},
     },
 }
