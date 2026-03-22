72d2b1bfaddc23ca9b5ac3ce76c047e2ce6bc7c6
Smith Alike <sojenir@gmail.com>
2026-03-22 13:03:54 +0800

    Update

--- lua/gionvim/plugins/lsp/langue/kulala.lua
+++ lua/gionvim/plugins/lsp/langue/kulala.lua
@@ -1,4 +1,4 @@
-vim.lsp.config("kulala-ls", {
+vim.lsp.config("kulala_ls", {
     filetypes = { "http", "rest" },
     capabilities = require("blink.cmp").get_lsp_capabilities({
         workspace = { fileOperations = { didRename = true, willRename = true } },
