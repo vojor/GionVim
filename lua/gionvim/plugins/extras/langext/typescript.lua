ae5f4f8b7a794b421dbbc5c634121145f97db2a0
Smith Alike <sojenir@gmail.com>
2026-03-28 01:51:31 +0800

    perf: oxlint rules elevate, tsserver use oxlint and biome lint,use biome format

--- lua/gionvim/plugins/extras/langext/typescript.lua
+++ lua/gionvim/plugins/extras/langext/typescript.lua
@@ -6,6 +6,18 @@ return {
         dependencies = { { "plenary.nvim" }, { "nvim-lspconfig" } },
         opts = {
             settings = {
+                on_attach = function(client, bufnr)
+                    client.server_capabilities.documentFormattingProvider = false
+                    client.server_capabilities.documentRangeFormattingProvider = false
+                end,
+                separate_diagnostic_server = false,
+                expose_as_code_action = {
+                    "fix_all",
+                    "add_missing_imports",
+                    "remove_unused",
+                    "organize_imports",
+                },
+                tsserver_max_memory = "4096",
                 tsserver_file_preferences = {
                     includeInlayParameterNameHints = "all",
                     includeCompletionsForModuleExports = true,
