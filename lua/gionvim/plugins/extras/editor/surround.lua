74353475c85e95c7691c8e874cf236b6076d31f8
Smith Alike <sojenir@gmail.com>
2026-04-18 00:56:57 +0800

    feat: use mini.surround replace nvim-surround

--- lua/gionvim/plugins/extras/editor/surround.lua
+++ lua/gionvim/plugins/extras/editor/surround.lua
@@ -1,9 +1,33 @@
 return {
     {
-        "kylechui/nvim-surround",
-        branch = "main",
+        "nvim-mini/mini.surround",
         lazy = true,
-        event = "LazyFile",
-        opts = {},
+        keys = function(_, keys)
+            local opts = GionVim.opts("mini.surround")
+            local mappings = {
+                { opts.mappings.add, desc = "Add Surrounding", mode = { "n", "x" } },
+                { opts.mappings.delete, desc = "Delete Surrounding" },
+                { opts.mappings.find, desc = "Find Right Surrounding" },
+                { opts.mappings.find_left, desc = "Find Left Surrounding" },
+                { opts.mappings.highlight, desc = "Highlight Surrounding" },
+                { opts.mappings.replace, desc = "Replace Surrounding" },
+                { opts.mappings.update_n_lines, desc = "Update `MiniSurround.config.n_lines`" },
+            }
+            mappings = vim.tbl_filter(function(m)
+                return m[1] and #m[1] > 0
+            end, mappings)
+            return vim.list_extend(mappings, keys)
+        end,
+        opts = {
+            mappings = {
+                add = "gsa",
+                delete = "gsd",
+                find = "gsf",
+                find_left = "gsF",
+                highlight = "gsh",
+                replace = "gsr",
+                update_n_lines = "gsn",
+            },
+        },
     },
 }
